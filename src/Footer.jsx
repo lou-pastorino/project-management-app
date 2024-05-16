3a42fb899ac0fe7fc4318f560669294e899ac3de
lou-pastorino <168196916+lou-pastorino@users.noreply.github.com>
2024-05-16 11:11:06 +0100

    Add additional styling

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,10 +2,10 @@ export default function Footer() {
   var today = new Date();
 
   return (
-    <footer className="container">
+    <footer className="container footer">
       <div className="row justify-content-center">
         <div className="col-8">
-          <h5>Team member allocation App - {today.getFullYear()}</h5>
+          <p>RocketStack Team Builder - {today.getFullYear()}</p>
         </div>
       </div>
     </footer>
